7dde843e09c46e6d430c362198b3a023f92d7704
Sangeeth Nandakumar <twileloop@outlook.com>
2024-01-21 14:30:43 +0530

    fix - Execute Js issue fixes

--- Twileloop.Spider.Demo/Program.cs
+++ Twileloop.Spider.Demo/Program.cs
@@ -8,6 +8,7 @@ using (IBrowser browser = new BrowserBuilder().SetUpChrome(
     browser.UseJQuery();
     browser.UseXPath();
 
+    //To execute and get result of JS 'Always add return'
     var test = browser.ExecuteJavaScript("return (1+1)");
 
     browser.Goto("https://libgen.is");
