c8cd6281d8a888a23023e672122412f17aa541fb
Sangeeth Nandakumar <twileloop@outlook.com>
2024-01-21 15:27:36 +0530

    fix - Support for killing all running Chrome drivers

--- Twileloop.Spider/BrowserBuilder.cs
+++ Twileloop.Spider/BrowserBuilder.cs
@@ -1,28 +1,32 @@
 ﻿using OpenQA.Selenium.Chrome;
 using WebDriverManager.DriverConfigs.Impl;
 using WebDriverManager;
+using System.Diagnostics;
 
 namespace Twileloop.Spider
 {
     public class BrowserBuilder
     {
-        public IBrowser SetUpChrome(bool openBrowser = true, bool incognito = true, string userProfilePath = null)
+        public IBrowser SetUpChrome(bool openBrowser = true, bool incognito = true, bool killAllChromeDrivers = false)
         {
-            return new Chrome(GetChromeDriver(openBrowser, incognito, userProfilePath));
+            if (killAllChromeDrivers)
+            {
+                Console.WriteLine("KILLING ALL CHROME DRIVERS");
+                Console.WriteLine("___________________________________________________________");
+                KillAllChromeDrivers("taskkill", "/F /IM chromedriver.exe /T");
+                Console.WriteLine("___________________________________________________________");
+            }
+            return new Chrome(GetChromeDriver(openBrowser, incognito));
         }
 
-        private ChromeDriver GetChromeDriver(bool openBrowser, bool incognito, string userProfilePath)
+        private ChromeDriver GetChromeDriver(bool openBrowser, bool incognito)
         {
             var options = new ChromeOptions
             {
                 AcceptInsecureCertificates = true,
             };
-
-            if (!string.IsNullOrEmpty(userProfilePath))
-            {
-                options.AddArguments($"--user-data-dir={userProfilePath}");
-            }
-            else if (incognito)
+            
+            if (incognito)
             {
                 options.AddArguments("--incognito");
             }
@@ -40,6 +44,37 @@ namespace Twileloop.Spider
             new DriverManager().SetUpDriver(new ChromeConfig());
             var newChrome = new ChromeDriver(service, options);
             return newChrome;
+        }
+
+        private void KillAllChromeDrivers(string command, string arguments)
+        {
+            try
+            {
+                ProcessStartInfo processInfo = new ProcessStartInfo
+                {
+                    FileName = command,
+                    Arguments = arguments,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                using (Process process = new Process { StartInfo = processInfo })
+                {
+                    process.Start();
+                    string output = process.StandardOutput.ReadToEnd();
+                    string error = process.StandardError.ReadToEnd();
+                    process.WaitForExit();
+
+                    Console.WriteLine("Output:\n" + output);
+                    Console.WriteLine("Error:\n" + error);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
         }
     }
 
